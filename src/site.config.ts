fcf6fb2fac35ece1ee46a9ded6144fd4da42fa53
YAMASHITA Hiromu <hirom.working@gmail.com>
2025-08-07 18:27:55 +0900

    first commit

--- src/site.config.ts
+++ src/site.config.ts
@@ -47,15 +47,15 @@ const config: SiteConfig = {
     mode: 'select',
     // The default theme identifier, used when themeMode is "select" or "light-dark-auto".
     // Make sure this is one of the themes listed in `themes` or "auto" for "light-dark-auto" mode.
-     default: 'catppuccin-mocha',
+    default: 'catppuccin-mocha',
     // Shiki themes to bundle with the site.
     // https://expressive-code.com/guides/themes/#using-bundled-themes
     // These will be used to theme the entire site along with syntax highlighting.
     // To use light-dark-auto mode, only include a light and a dark theme in that order.
     // include: [
-      //  'github-light',
-      //  'github-dark',
-    //]
+    //   'github-light',
+    //   'github-dark',
+    // ]
     include: [
       'andromeeda',
       'aurora-x',
