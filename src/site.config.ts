111b321861a7664b83ea469e3338c8ed96d68a4a
YAMASHITA Hiromu <mokipoint@gmail.com>
2025-08-08 20:48:17 +0900

    微修正

--- src/site.config.ts
+++ src/site.config.ts
@@ -11,7 +11,7 @@ const config: SiteConfig = {
   // The author of the site, used in the footer, SEO, and RSS feed.
   author: 'YAMASHITA Hiromu',
   // Keywords for SEO, used in the meta tags.
-  tags: ['Astro', 'NIKON_Z5Ⅱ', 'ポートレート撮影', 'スナップ撮影', '片岡奈央乃'],
+  tags: ['Astro', 'NIKON_Z5Ⅱ', 'ポートレート撮影', 'スナップ撮影'],
   // Path to the image used for generating social media previews.
   // Needs to be a square JPEG file due to limitations of the social card generator.
   // Try https://squoosh.app/ to easily convert images to JPEG.
