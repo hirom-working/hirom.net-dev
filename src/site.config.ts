7634444ec1f37b9b922b6675dcefa4c64296a421
YAMASHITA Hiromu <mokipoint@gmail.com>
2025-08-15 14:12:22 +0900

    フォントを変更しました

--- src/site.config.ts
+++ src/site.config.ts
@@ -18,7 +18,7 @@ const config: SiteConfig = {
   socialCardAvatarImage: './src/content/avatar.jpg',
   // Font imported from @fontsource or elsewhere, used for the entire site.
   // To change this see src/styles/global.css and import a different font.
-  font: 'Noto Sans JP',
+  font: 'Noto Sans JP ExtraLight',
   // For pagination, the number of posts to display per page.
   pageSize: 5,
   // The navigation links to display in the header.
