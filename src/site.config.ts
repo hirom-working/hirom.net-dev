b2171edbdbe3ae87b6844de11a906dde946e135e
YAMASHITA Hiromu <mokipoint@gmail.com>
2025-08-29 05:08:44 +0900

    フォントを変えたよ

--- src/site.config.ts
+++ src/site.config.ts
@@ -18,7 +18,7 @@ const config: SiteConfig = {
   socialCardAvatarImage: './src/content/avatar.jpg',
   // Font imported from @fontsource or elsewhere, used for the entire site.
   // To change this see src/styles/global.css and import a different font.
-  font: "Helvetica Neue",Arial,"Hiragino Kaku Gothic ProN","Hiragino Sans",Meiryo,sans-serif,
+  font: "Hiragino Kaku Gothic ProN",
   // For pagination, the number of posts to display per page.
   pageSize: 5,
   // The navigation links to display in the header.
